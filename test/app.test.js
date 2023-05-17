7f50f6d909f89d0addf4d67226cad8a114f8a213
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-05-17 13:38:57 +0200

    added to docker img publish

--- test/app.test.js
+++ test/app.test.js
@@ -5,7 +5,7 @@ const app = require('../app');
 describe('GET /', () => {
   it('Testing index endpoint', async () => {
     const response = await request(app).get('/');
-
+//
     expect(response.status).to.equal(200);
   });
 });
