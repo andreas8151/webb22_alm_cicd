af8cc4e915905c7d0045997e8dab748e924d3cc5
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-05-23 16:16:40 +0200

    feat(app.js): testing pipeline

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page here
+  // render the error page heree
   res.status(err.status || 500);
   res.render("error");
 });
